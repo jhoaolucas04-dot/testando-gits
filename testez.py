108c5c91ccb6f981fdacec30cd560e79e9ec600d
jhoaolucas04-dot <jhoaolucas04@gmail.com>
2026-05-21 11:31:19 -0300

    add:erro2

--- testez.py
+++ testez.py
@@ -1,26 +1,10 @@
-import math
- 
-def es_primo(numero):
-    """
-    Funcion que determina si un numero es primo
-    Tiene que recibir el numero entero
-    """
-    # Para que un numero sea primo, unicamente tiene que dividirse dos veces:
-    #   1 - divisible entre 1
-    #   2 - divisible entre el mismo
-    # En este bucle, empezamos por el dos hasta un numero anterior a el, por lo
-    # que si en el bucle, alguna vez se divide el numero, quiere decir que no es
-    # primo
-    if (numero<=1):
-        return False
- 
-    for i in range(2, math.ceil(math.sqrt(numero))+1):
-        if(numero%i==0 and i!=numero):
-            return False
-    return True
- 
-def numeroTop(num):
-    if type(num)!=int:
-        raise TypeError("Se esperaba un numero entero")
-    lst=[int(i) for i in str(num)]
-    return es_primo(sum(lst)/len(lst))
+print("um")
+print("um")
+print("um")
+print("um")
+print('um")
+print("um")
+print("um")
+print("um")
+print("um")
+print("um")
